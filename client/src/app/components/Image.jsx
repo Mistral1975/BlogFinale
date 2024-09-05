5044943364fbd536c3257f1ea0a351f6f16c86ca
Mistral1975 <162476054+Mistral1975@users.noreply.github.com>
2024-09-05 02:33:35 +0200

    Delete client/src directory

--- client/src/app/components/Image.jsx
+++
@@ -1,9 +0,0 @@
-// client/src/app/components/Image.jsx
-import NextImage from 'next/image';
-import React from 'react';
-
-const Image = (props) => {
-    return <NextImage {...props} />;
-};
-
-export default Image;
--- client/src/app/components/Logo.jsx
+++
@@ -1,7 +0,0 @@
-import logoImage from "../images/logo.svg";
-
-export const Logo = (props) => {
-    return (
-        <img src={logoImage.src} alt={props.alt} />
-    );
-}
