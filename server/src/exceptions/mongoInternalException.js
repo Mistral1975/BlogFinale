dadbe7cadc5cac8471014cd7f27f1fb4916064dd
Andrea <andreazucca@gmail.com>
2024-09-09 18:01:20 +0200

    inizio3

--- server/src/exceptions/mongoInternalException.js
+++
@@ -1,8 +0,0 @@
-import InternalServerException from './internalServerException.js';
-
-class MongoInternalException extends InternalServerException {
-    constructor(message, code){
-        super(message, code);
-    }
-}
-export default MongoInternalException;
