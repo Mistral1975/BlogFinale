ea4ef53b8c358ac04f7f0a02414c4718c82186d0
Andrea <andreazucca@gmail.com>
2024-09-13 10:01:31 +0200

    modifica route conferma registrazione

--- server/src/controllers/confirmRegistrationController.js
+++ server/src/controllers/confirmRegistrationController.js
@@ -3,7 +3,8 @@ import { confirmRegistration } from "../services/userService.js";
 export default async (req, res) => {
     try {
         await confirmRegistration(req.params.id, req.params.token);
-        res.status(200).send('ok');
+        //res.status(200).send('ok');
+        res.redirect('http://localhost:3000/');
     } catch(err) {
         res.status(err.status || 500);
         res.send(JSON.stringify({message: err.message, code: err.code}))
