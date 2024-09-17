7f1ef785ce52376ff49b02229c71048af2b17aff
Andrea <andreazucca@gmail.com>
2024-09-17 09:21:45 +0200

    completata la gestione dei likes lato BE e nello store di Redux con tutti i controlli

--- client/src/app/components/Like.jsx
+++ client/src/app/components/Like.jsx
@@ -1,31 +1,6 @@
 // components/Like.jsx
 "use client"
 
-//import React, { useState } from 'react'
-//import { AiFillLike } from 'react-icons/ai'
-
-/* function Like() {
-    const [liked, setLiked] = useState(false);
-    const [likeCount, setLikeCount] = useState(0);
-
-    const handleClick = () => {
-        if (liked) {
-            setLiked(false);
-            setLikeCount(likeCount - 1);
-        } else {
-            setLiked(true);
-            setLikeCount(likeCount + 1);
-        }
-    };
-
-    return (
-        <div onClick={handleClick} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
-            <AiFillLike color={liked ? "aqua" : "gray"} size="25" />
-            <span style={{ marginLeft: '10px', fontSize: '24px' }}>{likeCount}</span>
-        </div>
-    )
-} */
-
 import React from 'react';
 import { AiFillLike } from 'react-icons/ai';
 import { useDispatch, useSelector } from 'react-redux';
@@ -35,13 +10,22 @@ function Like({ postId }) {
     const dispatch = useDispatch();
     const user = useSelector(state => state.user); // Ottiene i dati dell'utente loggato
     const post = useSelector(state => state.postblog.postsList.find(post => post._id === postId)); // Recupera il post dal Redux store
-    //const liked = post?.likedBy.includes(user._id); // Verifica se l'utente ha già messo like
-    //const likeCount = post?.likes || 0; // Otteniamo il numero di like dal post
-
 
     if (!post) {
         console.error('Post non trovato nel Redux store per postId:', postId); // Log di errore se il post non è trovato
         return null;
+    }
+
+    // Controlla se l'utente è loggato
+    if (!user || !user._id) {
+        console.log('Utente non loggato, non può mettere like'); // Log per utente non loggato
+        return null; // Se l'utente non è loggato, non mostriamo il pulsante like
+    }
+
+    // Controlla se l'utente è il proprietario del post
+    if (post.userId._id === user._id) {
+        console.log('Proprietario del post, non può mettere like'); // Log per il proprietario del post
+        return null; // Se l'utente è il proprietario del post, non mostriamo il pulsante like
     }
 
     // Aggiungi un controllo per verificare se `likedBy` esiste prima di chiamare `.includes()`
