da18c382c20254bf2cd9ee2d6a98d290738a62d1
az1975 <andreazucca1975@gmail.com>
2024-09-23 18:22:08 +0200

    inizio refactoring

---
+++ client/src/app/layouts/SinglePostLayout copy.jsx
@@ -0,0 +1,210 @@
+// app/layouts/SinglePostLayout.jsx
+
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useRouter, useParams } from "next/navigation";
+import { setList, deletePost } from '../store/postsSlice';
+import Link from 'next/link';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import PostDate from '../components/PostDate';
+import Avatar from '../components/Avatar';
+import TagsOverviewSinglePost from '../components/TagsOverviewSinglePost';
+import Comments from '../components/Comments';
+import EditPostForm from '../components/EditPostForm';
+import PostActions from '../components/PostActions';
+import PostNavigation from '../components/PostNavigation';
+import DeleteConfirmModal from '../components/DeleteConfirmModal';
+
+
+const SinglePostLayout = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const router = useRouter(); // Usa il router di Next.js
+  const user = useSelector(state => state.user);
+  const loading = useSelector((state) => state.postblog.loading);
+  const error = useSelector((state) => state.postblog.error);
+  const postsList = useSelector(state => state.postblog.postsList);
+  const singlePost = postsList.find(post => post._id === id);
+  const currentIndex = postsList.findIndex(post => post._id === id);
+  const prevPost = currentIndex > 0 ? postsList[currentIndex - 1] : null;
+  const nextPost = currentIndex < postsList.length - 1 ? postsList[currentIndex + 1] : null;
+  const [showForm, setShowForm] = useState(false);
+  const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/list`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `bearer ${user.accessToken}`
+      },
+    }).then(async res => {
+      const jsonRes = await res.json();
+      dispatch(setList(jsonRes));
+    }).catch(error => {
+      console.error('Errore nel recuperare i post per tag:', error);
+    });
+  }, [id, dispatch, user.accessToken]);
+
+  if (loading) {
+    return <div>Caricamento...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!singlePost) {
+    return <div>Post non trovato</div>;
+  }
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/posts/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${user.accessToken}`
+        },
+      });
+
+      if (response.ok) {
+        dispatch(deletePost(id)); // Dispatch dell'azione Redux per eliminare il post
+        //closeDeleteConfirm(); // Chiudi la finestra di conferma dopo l'eliminazione del post
+        router.push('/blog'); // Reindirizza alla homepage
+      } else {
+        console.error('Failed to delete post');
+      }
+    } catch (error) {
+      console.error('Error deleting post:', error);
+    }
+  };
+  
+  //const [openModal, setOpenModal] = useState(false);    
+  //const handleOpenModal = () => setOpenModal(true);
+  const openForm = () => setShowForm(true);
+  const closeForm = () => setShowForm(false);
+  //const openDeleteConfirm = () => setShowDeleteConfirm(true);
+  //const closeDeleteConfirm = () => setShowDeleteConfirm(false);
+
+  return (
+    <>
+      <Header />
+      <main>
+        <article>
+          <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
+            <header className="pt-6 xl:pb-6">
+              <div className="space-y-1 text-center">
+                <div className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                  <time datetime={singlePost.createdAt}>
+                    <PostDate date={singlePost.createdAt} format="long" />
+                  </time>
+                </div>
+                <div>
+                  <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                    {singlePost.title}
+                  </h1>
+                </div>
+              </div>
+            </header>
+            <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
+              <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
+                <dt className="sr-only">Authors</dt>
+                <dd>
+                  <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+                    <li className="flex items-center space-x-2" key={singlePost.userId._id}>
+                      <Avatar user={singlePost.userId} />
+                      <dl className="whitespace-nowrap text-sm font-medium leading-5">
+                        <dt className="sr-only">Name</dt>
+                        <dd className="text-gray-900 dark:text-gray-100">{singlePost.userId.displayName}</dd>
+                      </dl>
+                    </li>
+                  </ul>
+                </dd>
+              </dl>
+              <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+                <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">
+                  {singlePost.imageUrl &&
+                    <img src={singlePost.imageUrl} alt={singlePost.title} className="w-full h-auto mb-8" />
+                  }
+                  {!singlePost.imageUrl &&
+                    <div></div>
+                  }
+                  {singlePost.description}</div>
+                {user.email && user._id === singlePost.userId._id && !showForm &&
+                  <PostActions
+                  onEdit={() => setShowForm(true)}
+                  onDelete={() => setShowDeleteConfirm(true)}
+                />
+                }
+                {/********* MODIFICA POST ********/}
+                {showForm && (
+                  <EditPostForm singlePost={singlePost} user={user} closeForm={closeForm} />
+                )}
+                {/******* COMMENTI DEL POST ******/}
+                <Comments postId={singlePost._id} />
+              </div>
+              <footer>
+                <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      <TagsOverviewSinglePost postId={singlePost._id} />
+                    </div>
+                  </div>
+                  {(nextPost || prevPost) && (
+                    <PostNavigation prevPost={prevPost} nextPost={nextPost} />
+                  )}
+                </div>
+                <div className="pt-4 xl:pt-8">
+                  <Link
+                    href={`/blog`}
+                    className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                    aria-label="Back to the blog"
+                  >
+                    &larr; Back to the blog
+                  </Link>
+                </div>
+              </footer>
+            </div>
+          </div>
+        </article>
+      </main >
+      <Footer />
+
+
+      {/********************************/}
+      {/** FINESTRA DI CONFERMA ELIMINAZIONE **/}
+      {/********************************/}
+      {/* {showDeleteConfirm && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-white p-8 rounded-lg shadow-lg dark:bg-gray-700">
+            <h2 className="text-xl font-semibold mb-4 dark:text-white">Conferma Eliminazione</h2>
+            <p className="mb-4 dark:text-gray-300">Sei sicuro di voler eliminare il post: <strong>{singlePost.title}</strong>?</p>
+            <div className="flex justify-end">
+              <button onClick={handleDelete} className="bg-red-500 text-white px-4 py-2 rounded mr-2">Elimina</button>
+              <button onClick={closeDeleteConfirm} className="bg-gray-300 text-gray-700 px-4 py-2 rounded">Annulla</button>
+            </div>
+          </div>
+        </div>
+      )} */}
+      {showDeleteConfirm && (
+        <DeleteConfirmModal
+          postTitle={singlePost.title}
+          onDelete={handleDelete}
+          onCancel={() => setShowDeleteConfirm(false)}
+        />
+      )}
+      {/********************************/}
+      {/************* FINE *************/}
+      {/********************************/}
+
+
+    </>
+  )
+}
+
+export default SinglePostLayout;
--- client/src/app/store/commentsSlice.jsx
+++ client/src/app/store/commentsSlice.jsx
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    commentsList: [],
     comments: {}, // Oggetto per memorizzare i commenti per postId
     commentsCount: {}, // Oggetto per memorizzare il conteggio dei commenti per postId
 };
@@ -10,35 +9,61 @@ const commentsSlice = createSlice({
     name: "comments",
     initialState,
     reducers: {
-        setComments: (state, action) => {
-            const { postId, comments } = action.payload;
-            state.comments[postId] = comments;
-        },
-        setCommentsCount: (state, action) => {
-            const { postId, commentsCount } = action.payload;
-            state.commentsCount[postId] = commentsCount;
-        },
         addComment: (state, action) => {
-            //console.log(`Sto aggiungento un COMMENTO alla commentsList su REDUX`);
-            state.commentsList.push(action.payload);
+            const { postId, comment } = action.payload;
+
+            if (!state.comments[postId]) {
+                state.comments[postId] = [];
+            }
+            state.comments[postId].push(comment);
+
+            // Aggiorna il conteggio dei commenti
+            if (!state.commentsCount[postId]) {
+                state.commentsCount[postId] = 0;
+            }
+            state.commentsCount[postId] += 1;
         },
-        updateComment: (state, action) => {
-            const updateComment = action.payload;
-            const index = state.commentsList.findIndex(comment => comment._id === updateComment._id);
-            if (index !== -1) {
-                state.commentsList[index] = {
-                    ...state.commentsList[index], // Mantieni i dettagli esistenti
-                    ...updateComment,
-                    userId: state.commentsList[index].userId // Mantieni i dettagli dell'utente
-                };
+
+        deleteComment: (state, action) => {
+            const { postId, commentId } = action.payload;
+
+            if (state.comments[postId]) {
+                state.comments[postId] = state.comments[postId].filter(
+                    comment => comment.id !== commentId
+                );
+
+                // Aggiorna il conteggio dei commenti
+                if (state.commentsCount[postId]) {
+                    state.commentsCount[postId] -= 1;
+                }
             }
         },
-        deleteComment: (state, action) => {
-            //console.log(`Sto eliminando un COMMENTO dalla commentsList su REDUX`);
-            state.commentsList = state.commentsList.filter(item => item._id !== action.payload);
+
+        editComment: (state, action) => {
+            const { postId, commentId, updatedComment } = action.payload;
+
+            if (state.comments[postId]) {
+                const commentIndex = state.comments[postId].findIndex(
+                    comment => comment.id === commentId
+                );
+
+                if (commentIndex !== -1) {
+                    state.comments[postId][commentIndex] = {
+                        ...state.comments[postId][commentIndex],
+                        ...updatedComment
+                    };
+                }
+            }
         },
+
+        setComments: (state, action) => {
+            const { postId, comments } = action.payload;
+
+            state.comments[postId] = comments;
+            state.commentsCount[postId] = comments.length;
+        }
     },
 });
 
-export const { setComments, setCommentsCount, addComment, updateComment, deleteComment } = commentsSlice.actions;
+export const { addComment, deleteComment, editComment, setComments } = commentsSlice.actions;
 export default commentsSlice.reducer;
